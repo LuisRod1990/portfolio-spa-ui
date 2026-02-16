db7cbc7defa879642df6043a6ce0941185355c6f
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-16 04:45:13 -0600

    Actualización - Se agregan logs al AuthGuard para monitorear sesion (están deshabilitados)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  authApi: 'https://localhost:44339/api/auth',
-  dataApi: 'https://localhost:44347/api/Portfolio',
+  authApi: 'https://portfolio-lrguez.duckdns.org/api/auth',
+  dataApi: 'https://portfolio-lrguez.duckdns.org/api/portfolio',
   
   endpoints: {
     login: '/login',
