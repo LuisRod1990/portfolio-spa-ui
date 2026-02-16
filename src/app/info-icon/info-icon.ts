5cc619242a7b63735044ca1bed7c6c07f66e944c
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-16 08:20:54 -0600

    Actualización - Botón de información

--- src/app/info-icon/info-icon.ts
+++ src/app/info-icon/info-icon.ts
@@ -1,15 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-info-icon',
   standalone: true,
-  imports: [CommonModule, MatTooltipModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './info-icon.html',
   styleUrls: ['./info-icon.scss']
 })
-export class InfoIconComponent {
+export class InfoIconComponent implements OnInit {
   @Input() tooltipText: string = 'Información';
+  showLabel: boolean = true;
+  fadingOut: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    // Mantener visible 2 segundos
+    setTimeout(() => {
+      this.fadingOut = true;
+      this.cdr.detectChanges(); // fuerza actualización inmediata
+
+      // esperar a que termine la animación antes de quitar del DOM
+      setTimeout(() => {
+        this.showLabel = false;
+        this.cdr.detectChanges(); // vuelve a refrescar
+      }, 500); // duración del fade-out
+    }, 1000); // tiempo visible antes de desaparecer
+  }
 }
